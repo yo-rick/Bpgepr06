f7c58065977d1e7023e3feb562ca8062acb30a24
yo-rick <schrijfaan@me.com>
2016-09-25 19:05:28 +0200

    Add files via upload

--- pytest.py
+++ pytest.py
@@ -3,6 +3,10 @@ def verdubbel(getal1, getal2):
     dubbel2 = getal2 * 2
     return dubbel1, dubbel2
 
+def keer(getal1, getal2):
+    keer1 = getal1 * getal2
+    return keer1
+
 
 def main():
     print("Dit programma doet iets met getallen.")
@@ -11,6 +15,8 @@ def main():
     uitkomst1, uitkomst2 = verdubbel(vraag1, vraag2)
     print('getal 1', uitkomst1)
     print("Getal 2 dubbel =", uitkomst2)
+    getalkeer = keer(vraag1, vraag2)
+    print("het getaal keer het andere getal =", getalkeer)
 
 
 main()
