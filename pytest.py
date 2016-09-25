7684e5a0328576792dce4aad0d44991c4b9a55eb
yo-rick <schrijfaan@me.com>
2016-09-25 19:02:22 +0200

    Add files via upload

---
+++ pytest.py
@@ -0,0 +1,16 @@
+def verdubbel(getal1, getal2):
+    dubbel1 = getal1 * 2
+    dubbel2 = getal2 * 2
+    return dubbel1, dubbel2
+
+
+def main():
+    print("Dit programma doet iets met getallen.")
+    vraag1 = int(input("Geef een getal op: "))
+    vraag2 = int(input("Geef nog een getal op: "))
+    uitkomst1, uitkomst2 = verdubbel(vraag1, vraag2)
+    print('getal 1', uitkomst1)
+    print("Getal 2 dubbel =", uitkomst2)
+
+
+main()
